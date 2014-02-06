122e2c854c05348bc151bbd98b35cda984ff7263
Colin Stone <colin@bunnylaundering.com>
2014-02-05 23:27:53 -0800

    tweaking some includes

--- software/fxgame/animations.c
+++ software/fxgame/animations.c
@@ -1,4 +1,3 @@
-#include <string.h>
 #include "animations.h"
 #include "readbmp.h"
 #include "sdcard.h"
--- software/fxgame/animations.h
+++ software/fxgame/animations.h
@@ -4,8 +4,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <system.h>
+#include <string.h>
 
-#include "readbmp.h"
 #include "queue_type.h"
 #include "altera_up_avalon_video_pixel_buffer_dma.h"
 #include "altera_up_avalon_video_character_buffer_with_dma.h"
